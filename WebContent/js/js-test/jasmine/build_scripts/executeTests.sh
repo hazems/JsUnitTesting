23db8c7728d8d639f24c4e02b738fb1e38a0b469
hazems <hazems@apache.org>
2014-05-03 04:42:01 +0200

    Enhancing Build Integration

--- WebContent/js/js-test/jasmine/build_scripts/executeTests.sh
+++ WebContent/js/js-test/jasmine/build_scripts/executeTests.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-cd ..
-cd ..
-cd ..
+#CHANGE_THIS_ACCORDING_TO_YOUR_ENV
+JS_TEST_PATH="/Users/ibm/git/JsUnitTesting/WebContent/js"
+cd "$JS_TEST_PATH/js-test/jasmine/build_scripts"
+export PATH=$PATH:/usr/local/bin
+cd $JS_TEST_PATH
 # Execute command once !!!
 karma start config.js --single-run
